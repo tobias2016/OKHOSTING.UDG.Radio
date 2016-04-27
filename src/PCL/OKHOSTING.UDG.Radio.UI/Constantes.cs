97e7589402b69401c4238cea6e3ef5c11b5179a5
Edgard David Iván Muñoz Chávez <david@okhosting.com>
2016-04-27 13:25:24 -0500

    Agregar clase constantes

---
+++ src/PCL/OKHOSTING.UDG.Radio.UI/Constantes.cs
@@ -0,0 +1,85 @@
+﻿using OKHOSTING.UI;
+using OKHOSTING.UI.Controls;
+using OKHOSTING.UI.Controls.Layout;
+using System;
+using System.Collections.Generic;
+
+namespace OKHOSTING.UDG.Radio.UI
+{
+	public static class Constantes
+	{
+		public static readonly Color FontColor1 = new Color(255, 0, 0, 0);
+		public static readonly Color FontColor2 = new Color(255, 20, 20, 20);
+		public static readonly Color FontColor3 = new Color(255, 50, 50, 50);
+
+		public static int FontSize1
+		{
+			get
+			{
+				return 16;
+			}
+		}
+
+		public static int FontSize2
+		{
+			get
+			{
+				return 14;
+			}
+		}
+
+		public static int FontSize3
+		{
+			get
+			{
+				return 12;
+			}
+		}
+
+		public static double AnchoIconos
+		{
+			get
+			{
+				double ancho = Platform.Current.Page.Width * .2;
+
+				if (ancho > 70)
+				{
+					ancho = 70;
+				}
+
+				return ancho;
+			}
+		}
+
+		public const string FontFamily = "Arial";
+
+		public static IGrid CrearMenuVacio()
+		{
+			IGrid grdMenu = Platform.Current.Create<IGrid>();
+			grdMenu.RowCount = 3;
+			grdMenu.ColumnCount = 4;
+			grdMenu.Height = 60;
+			grdMenu.Width = Platform.Current.Page.Width;
+			grdMenu.BackgroundColor = new Color(255, 50, 50, 50);
+
+			return grdMenu;
+		}
+
+		public static ILabel CrearTitulo(string texto, Color fondo)
+		{
+			ILabel lblTitulo = Platform.Current.Create<ILabel>();
+			lblTitulo.Text = texto;
+			lblTitulo.Width = Platform.Current.Page.Width;
+			lblTitulo.FontFamily = Constantes.FontFamily;
+			lblTitulo.FontSize = Constantes.FontSize1;
+			lblTitulo.Height = 70;
+			lblTitulo.Bold = true;
+			lblTitulo.BackgroundColor = fondo;
+			lblTitulo.FontColor = Constantes.FontColor1;
+			lblTitulo.TextHorizontalAlignment = HorizontalAlignment.Center;
+			lblTitulo.TextVerticalAlignment = VerticalAlignment.Center;
+
+			return lblTitulo;
+		}
+	}
+}
