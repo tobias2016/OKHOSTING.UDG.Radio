a0ced4ae0604dc6e7dc5c19ddb72a4d1a585426a
Edgard David Iván Muñoz Chávez <david@okhosting.com>
2016-04-28 19:36:34 -0500

    quitar referencia al orm

--- src/PCL/OKHOSTING.UDG.Radio.UI/IndexController.cs
+++ src/PCL/OKHOSTING.UDG.Radio.UI/IndexController.cs
@@ -1,11 +1,3 @@
-using System;
-using System.Collections.Generic;
-using OKHOSTING.UI;
-using OKHOSTING.ORM;
-using OKHOSTING.UI.Controls;
-using OKHOSTING.UI.Controls.Layout;
-using OKHOSTING.Streaming;
-
 namespace OKHOSTING.UDG.Radio.UI
 { 
     public class IndexController : OKHOSTING.UI.Controller
@@ -16,8 +8,8 @@ namespace OKHOSTING.UDG.Radio.UI
         public override void Start()
         {
 			base.Start();
-            OKHOSTING.ORM.DataBase.Setup += DataBase_Setup;
-            DataType dtype = DataType.DefaultMap(typeof(Station));
+            //OKHOSTING.ORM.DataBase.Setup += DataBase_Setup;
+            //DataType dtype = DataType.DefaultMap(typeof(Station));
 
             //using (var db = DataBase.CreateDataBase())
             //{
@@ -34,9 +26,9 @@ namespace OKHOSTING.UDG.Radio.UI
             new SplashScreenScontroller().Start();
         }
 
-        private static ORM.DataBase DataBase_Setup()
-        {
-            return new ORM.DataBase(new Sql.Web.Services.Client.DataBase() { ConnectionString = "http://radioudg.okhosting.com/databaseservice.asmx" }, new Sql.MySql.SqlGenerator());
-        }
+        //private static ORM.DataBase DataBase_Setup()
+        //{
+        //    return new ORM.DataBase(new Sql.Web.Services.Client.DataBase() { ConnectionString = "http://radioudg.okhosting.com/databaseservice.asmx" }, new Sql.MySql.SqlGenerator());
+        //}
     }
 }
